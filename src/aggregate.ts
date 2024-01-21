9515ec5958aee348048974a4cd1941d71e22b8f7
Serge Kovbasiuk <isserge@protonmail.com>
2024-01-21 14:54:34 +0700

    Add jsdoc comments for `aggregate.ts`

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -2,8 +2,66 @@ import { graphql } from '@octokit/graphql';
 import { fetchRepoIssues, fetchRepoPullRequests } from "./github";
 import { RepositoryNode, IssueNode, PullRequestNode } from "./types";
 
+interface IssueMetrics {
+  openIssuesCount: number;
+  closedIssuesCount: number;
+  totalTimeToClose: number;
+  totalCommentsPerIssue: number;
+}
+
+function initializeIssueMetrics(): IssueMetrics {
+  return {
+    openIssuesCount: 0,
+    closedIssuesCount: 0,
+    totalTimeToClose: 0,
+    totalCommentsPerIssue: 0,
+  };
+}
+
+interface PRMetrics {
+  openPRsCount: number;
+  mergedPRsCount: number;
+  totalTimeToMerge: number;
+  totalCommentsPerPR: number;
+}
+
+function initializePRMetrics(): PRMetrics {
+  return {
+    openPRsCount: 0,
+    mergedPRsCount: 0,
+    totalTimeToMerge: 0,
+    totalCommentsPerPR: 0,
+  };
+}
+
 function millisecondsToDays(milliseconds: number) {
   return milliseconds / 1000 / 60 / 60 / 24;
+}
+
+function getOpenAndClosedIssues(issues: IssueNode[]) {
+  return issues.reduce((acc: [IssueNode[], IssueNode[]], issue) => {
+    if (issue.state === 'OPEN') {
+      acc[0].push(issue);
+    } else {
+      acc[1].push(issue);
+    }
+    return acc;
+  }, [[], []]);
+}
+
+function getOpenAndMergedPrs(pullRequests: PullRequestNode[]) {
+  return pullRequests.reduce((acc: [PullRequestNode[], PullRequestNode[]], pr) => {
+    if (pr.state === 'OPEN') {
+      acc[0].push(pr);
+    } else if (pr.state === 'MERGED') {
+      acc[1].push(pr);
+    }
+    return acc;
+  }, [[], []]);
+}
+
+function calculateAverage(metric: number, count: number) {
+  return count === 0 ? 0 : metric / count;
 }
 
 interface AggregatedData {
@@ -30,6 +88,14 @@ interface AggregatedData {
   };
 }
 
+/**
+ * Aggregate data from GitHub API
+ * @param client - Octokit graphql client
+ * @param org - Organization name
+ * @param reposData - Repositories data
+ * @param since - Date since which data is to be fetched
+ * @returns - Aggregated data
+ */
 export async function aggregateData(client: typeof graphql, org: string, reposData: { activeRepos: RepositoryNode[]; totalRepos: number; }, since: Date): Promise<AggregatedData> {
   let totalStars = 0;
   let totalForks = 0;
@@ -88,64 +154,12 @@ export async function aggregateData(client: typeof graphql, org: string, reposDa
   };
 }
 
-function getOpenAndClosedIssues(issues: IssueNode[]) {
-  return issues.reduce((acc: [IssueNode[], IssueNode[]], issue) => {
-    if (issue.state === 'OPEN') {
-      acc[0].push(issue);
-    } else {
-      acc[1].push(issue);
-    }
-    return acc;
-  }, [[], []]);
-}
-
-function getOpenAndMergedPrs(pullRequests: PullRequestNode[]) {
-  return pullRequests.reduce((acc: [PullRequestNode[], PullRequestNode[]], pr) => {
-    if (pr.state === 'OPEN') {
-      acc[0].push(pr);
-    } else if (pr.state === 'MERGED') {
-      acc[1].push(pr);
-    }
-    return acc;
-  }, [[], []]);
-}
-
-function calculateAverage(metric: number, count: number) {
-  return count === 0 ? 0 : metric / count;
-}
-
-interface IssueMetrics {
-  openIssuesCount: number;
-  closedIssuesCount: number;
-  totalTimeToClose: number;
-  totalCommentsPerIssue: number;
-}
-
-function initializeIssueMetrics(): IssueMetrics {
-  return {
-    openIssuesCount: 0,
-    closedIssuesCount: 0,
-    totalTimeToClose: 0,
-    totalCommentsPerIssue: 0,
-  };
-}
-
-interface PRMetrics {
-  openPRsCount: number;
-  mergedPRsCount: number;
-  totalTimeToMerge: number;
-  totalCommentsPerPR: number;
-}
-
-function initializePRMetrics(): PRMetrics {
-  return {
-    openPRsCount: 0,
-    mergedPRsCount: 0,
-    totalTimeToMerge: 0,
-    totalCommentsPerPR: 0,
-  };
-}
-
+/**
+ * Process repo issues and update metrics
+ * @param issues - Issues
+ * @param metrics - Issue metrics
+ * @returns - Updated metrics
+ */
 function processIssues(issues: IssueNode[], metrics: IssueMetrics): IssueMetrics {
   const [open, closed] = getOpenAndClosedIssues(issues);
   metrics.openIssuesCount += open.length;
@@ -161,6 +175,12 @@ function processIssues(issues: IssueNode[], metrics: IssueMetrics): IssueMetrics
   return metrics;
 }
 
+/**
+ * Process repo pull requests and update metrics
+ * @param pullRequests - Pull requests
+ * @param metrics - Pull request metrics
+ * @returns - Updated metrics
+ */
 function processPullRequests(pullRequests: PullRequestNode[], metrics: PRMetrics) {
   const [open, merged] = getOpenAndMergedPrs(pullRequests);
   metrics.openPRsCount += open.length;
