3fdcf699fe0866402eff36c175d2e880ee7c965a
Serge Kovbasiuk <isserge@protonmail.com>
2024-01-22 16:43:48 +0700

    Add tests for fetching organization repositories

---
+++ src/test/github.test.ts
@@ -0,0 +1,92 @@
+import { it } from 'node:test';
+import * as assert from 'node:assert/strict';
+import { GithubOrg } from '../github';
+import { graphql } from '@octokit/graphql';
+
+const since = new Date();
+
+since.setDate(since.getDate() - 7);
+
+const mockRepo = {
+  node: {
+    name: 'Repo1',
+    description: 'Repo1 description',
+    url: '',
+    stargazerCount: 1,
+    forkCount: 1,
+    pushedAt: since.toISOString(),
+  },
+  cursor: 'cursor1',
+}
+
+it('fetchOrganizationRepos should return a list of repositories', async () => {
+  const mockRepos = [mockRepo];
+
+  const mockClient = async () => {
+    return {
+      organization: {
+        repositories: {
+          edges: mockRepos,
+          pageInfo: { hasNextPage: false, endCursor: 'cursor1' }
+        }
+      }
+    }
+  };
+
+  const githubOrg = new GithubOrg(mockClient as unknown as typeof graphql, 'testOrg', since);
+  const result = await githubOrg.fetchOrganizationRepos();
+
+  assert.ok(result);
+  assert.equal(result.totalRepos, mockRepos.length);
+  assert.equal(result.activeRepos.length, mockRepos.length);
+});
+
+it('fetchOrganizationRepos should throw error if response validation fails', async () => {
+  const mockClient = async () => {
+    return {
+      incorrectResponseObject: {}
+    }
+  };
+
+  const githubOrg = new GithubOrg(mockClient as unknown as typeof graphql, 'testOrg', since);
+
+  try {
+    await githubOrg.fetchOrganizationRepos();
+    assert.fail('Expected method to throw an error');
+  } catch (error) {
+    assert.ok(error);
+  }
+});
+
+it('fetchOrganizationRepos should handle pagination correctly', async () => {
+  let callCount = 0;
+
+  const mockClient = async () => {
+    callCount++;
+    return {
+      organization: {
+        repositories: {
+          edges: [
+            {
+              node: {
+                ...mockRepo.node,
+                name: `Repo${callCount}`,
+              },
+              cursor: `cursor${callCount}`
+            },
+          ],
+          pageInfo: { hasNextPage: callCount < 2, endCursor: `cursor${callCount}` }
+        }
+      }
+    };
+  };
+
+  const githubOrg = new GithubOrg(mockClient as unknown as typeof graphql, 'testOrg', since);
+  const result = await githubOrg.fetchOrganizationRepos();
+
+  assert.ok(result);
+  assert.equal(callCount, 2);
+});
+
+
+
