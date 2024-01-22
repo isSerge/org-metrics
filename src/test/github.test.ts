ff335bc713a72e10d44ca37508e01d45b81be7ea
Serge Kovbasiuk <isserge@protonmail.com>
2024-01-22 20:14:14 +0700

    Add tests for fetching repository issues

--- src/test/github.test.ts
+++ src/test/github.test.ts
@@ -20,6 +20,18 @@ const mockRepo = {
   cursor: 'cursor1',
 }
 
+const mockIssue = {
+  title: 'Issue1',
+  url: '',
+  comments: { totalCount: 1 },
+  createdAt: since.toISOString(),
+  labels: { totalCount: 1 },
+  number: 1,
+  state: 'OPEN',
+  closedAt: null,
+  updatedAt: since.toISOString(),
+};
+
 const silentLogger = pino({
   level: 'silent'
 });
@@ -109,5 +121,54 @@ it('fetchOrganizationRepos should handle pagination correctly', async () => {
   assert.equal(callCount, 2);
 });
 
+it('fetchRepoIssues should return a list of issues', async () => {
+  const mockIssues = [mockIssue];
+
+  const mockClient = async () => {
+    return {
+      repository: {
+        issues: {
+          nodes: mockIssues,
+          pageInfo: { hasNextPage: false, endCursor: 'cursor1' },
+          totalCount: mockIssues.length,
+        }
+      }
+    }
+  };
+
+  const githubOrg = new GithubOrg({
+    client: mockClient as unknown as typeof graphql,
+    org: 'testOrg',
+    since,
+    logger: silentLogger,
+  });
+
+  const result = await githubOrg.fetchRepoIssues('Repo1');
+
+  assert.ok(result);
+  assert.equal(result.length, mockIssues.length);
+});
+
+it('fetchRepoIssues should throw error if response validation fails', async () => {
+  const mockClient = async () => {
+    return {
+      incorrectResponseObject: {}
+    }
+  };
+
+  const githubOrg = new GithubOrg({
+    client: mockClient as unknown as typeof graphql,
+    org: 'testOrg',
+    since,
+    logger: silentLogger,
+  });
+
+  try {
+    await githubOrg.fetchRepoIssues('Repo1');
+    assert.fail('Expected method to throw an error');
+  } catch (error) {
+    assert.ok(error);
+  }
+});
 
 
